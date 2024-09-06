45b0301c08839b410b9c09ab318cb4221e785b67
Yusrandi <yusrandhyr@gmail.com>
2024-09-06 22:20:31 +0700

    finishing

---
+++ components/Home/MyName/MyName.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+export default function MyName(props: { finishedLoading: boolean }) {
+  const router = useRouter();
+  return (
+    <div
+      className="h-full flex flex-col justify-center
+      px-8 2xl:px-72 xl:px-56 lg:px-32  md:px-28 sm:px-8 py-32 sm:py-52  "
+    >
+      <motion.span
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.4, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.4, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-AAsecondary font-mono"
+      >
+        Hi, my name is
+      </motion.span>
+      <motion.h1
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.5, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.5, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-300 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4"
+      >
+        Yusrandi.
+      </motion.h1>
+      <motion.h2
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.6, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.6, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-400 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4"
+      >
+        Software Engineer.
+      </motion.h2>
+
+      <motion.h3
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-400 font-Header text-sm md:text-lg sm:text-md mt-10 tracking-wider"
+      >
+        I&apos;m a <span className="text-AAsecondary">Software Developer</span>, with extensive experience in creating <span className="text-AAsecondary">Mobile Applications</span> and <span className="text-AAsecondary">web-based solutions</span>.
+        In my current role, I have successfully developed mobile applications for both <span className="text-AAsecondary">Android and iOS</span>, which are published on <span className="text-AAsecondary">Google Play Store and Apple App Store</span>.
+        I also have expertise in building effective web dashboards for monitoring and data management, as well as developing <span className="text-AAsecondary">RESTful APIs</span> to ensure seamless integration between mobile applications and backend systems.
+
+        {/* <br className="2xl:block hidden" />creating and deploying them, as well as implementing the
+        front-end components to enable seamless user interactions. */}
+      </motion.h3>
+      <motion.div
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.8, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.8, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="mt-12"
+      >
+        <a href={"/resume.pdf"} target={"_blank"} rel="noreferrer">
+          <button className="bg-AAprimary text-AAsecondary border rounded px-4 sm:px-8 py-3 sm:py-4 border-AAsecondary">
+            Check out my resume!
+          </button>
+        </a>
+      </motion.div>
+    </div>
+  );
+}
